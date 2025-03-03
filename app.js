7c360877dc69f89dd6fc848ddd38fd66116e028a
tonmoy <tonmoy3673@gmail.com>
2025-03-03 22:49:16 +0600

    :bulb: set characters

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('Connected');
+const passwordField= document.getElementById('Password')
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const specialSymbols = '!@#$%^&*()_+-=[]{}|;:,.<>?/~`';
+const length=12;
+
