125a90dc07ec26afea9f6248c0da676817c9e589
tonmoy <tonmoy3673@gmail.com>
2025-03-03 23:29:56 +0600

    :bulb: set upper and lower case

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ const numbers = '0123456789';
 const specialSymbols = '!@#$%^&*()_+-=[]{}|;:,.<>?/~`';
 const length=12;
 
+const createPassword =()=>{
+    let password='';
+    password +=uppercase[Math.floor(Math.random() * uppercase.length)]
+    password +=lowercase[Math.floor(Math.random() *lowercase.length)]
+}
