28f1d1d89973f4073fb95a6b18ab9da409e03fc9
tonmoy <tonmoy3673@gmail.com>
2025-03-05 21:12:20 +0600

    :bulb: implement generate password

--- app.js
+++ app.js
@@ -8,5 +8,14 @@ const length=12;
 const createPassword =()=>{
     let password='';
     password +=uppercase[Math.floor(Math.random() * uppercase.length)]
-    password +=lowercase[Math.floor(Math.random() *lowercase.length)]
+    password +=lowercase[Math.floor(Math.random() * lowercase.length)]
+    password +=numbers[Math.floor(Math.random() * numbers.length)]
+    password +=specialSymbols[Math.floor(Math.random() * specialSymbols.length)]
+    while (password.length < length) {
+        let setPass= uppercase + lowercase + numbers + specialSymbols;
+        password += setPass[Math.floor(Math.random() * setPass.length)]
+        
+    }
+    console.log(password);
 }
+
